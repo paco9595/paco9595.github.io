5c83b1e322e37539c71d6eb514f418de3f90ac3e
Francisco VIllarreal <paco_vip95@hotmail.com>
2024-04-07 10:56:22 -0600

    create Profetional Experience

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,5 @@
 window.addEventListener("scroll", (e) => {
-    console.log(e)
-  if (e.target.scrollingElement.scrollTop === 0) {
+  if (e.target.scrollingElement.scrollTop < 250) {
     document.querySelector('.scroll').classList.remove('hidden')
   }else {
     document.querySelector('.scroll').classList.add('hidden')
